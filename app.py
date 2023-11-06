b340deaf21737680d6595ce6688686863ce55c79
Mattral <88831350+Mattral@users.noreply.github.com>
2023-11-06 13:10:20 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import random
 import time
+from streamlit.ReportThread import get_report_ctx
 
 # Set a dark theme for the Streamlit app
 st.markdown(
@@ -35,35 +36,48 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# Whack-a-Mole Game
 st.title("Whack-a-Mole Game")
 
+# Game parameters
 grid_size = 4
-mole_position = (0, 0)
-score = 0
 game_over = False
+score = 0
+active_mole = None
+duration = 0.5  # Duration for mole appearance
 
+# Create a grid
+grid = [[False for _ in range(grid_size)] for _ in range(grid_size)]
+
+# Function to update the game
 def update_game():
-    global mole_position, score, game_over
-    mole_position = (random.randint(0, grid_size-1), random.randint(0, grid_size-1))
+    global active_mole, score
+    active_mole = (random.randint(0, grid_size - 1), random.randint(0, grid_size - 1))
     score = 0
-    game_over = False
+
+# Start a new game
+update_game()
 
 st.text("Click on the moles as quickly as possible!")
 
 if st.button("Start Game"):
     update_game()
 
+# Create a custom grid for the game
 st.markdown("<div class='custom-grid'>", unsafe_allow_html=True)
 for row in range(grid_size):
     for col in range(grid_size):
-        if game_over and (row, col) == mole_position:
+        # Handle the active mole
+        if game_over and (row, col) == active_mole:
             st.button(f"Mole\nScore: {score}", key=(row, col))
         elif st.button("", key=(row, col)):
-            if (row, col) == mole_position:
+            if (row, col) == active_mole:
                 score += 1
             update_game()
+
 st.markdown("</div>", unsafe_allow_html=True)
 
+# Game over condition
 if score >= 10:
     st.text("Congratulations! You win!")
 elif game_over:
